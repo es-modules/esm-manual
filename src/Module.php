1e0aab7e1aea43a96b3ab5f6e5439031882fdf98
Damon Smith <damon.easy.system@gmail.com>
2016-05-26 21:05:27 +0300

    add Module.php

---
+++ src/Module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the "ES Modules" package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author  Damon Smith <damon.es.modules@gmail.com>
+ */
+namespace Esm\Manual;
+
+use Es\Modules\AbstractModule;
+
+class Module extends AbstractModule
+{
+}
